632099a8c69652e394196ffee59014e8ed4b061a
Michel Fortin <michel.fortin@michelf.com>
2016-10-10 13:52:28 -0400

    Fixing missing ; in .inc.php files.

--- Michelf/SmartyPants.inc.php
+++ Michelf/SmartyPants.inc.php
@@ -6,4 +6,4 @@
 // Take a look at the PSR-0-compatible class autoloading implementation
 // in the Readme.php file if you want a simple autoloader setup.
 
-require_once dirname(__FILE__) . '/SmartyPants.php'
+require_once dirname(__FILE__) . '/SmartyPants.php';
--- Michelf/SmartyPantsTypographer.inc.php
+++ Michelf/SmartyPantsTypographer.inc.php
@@ -6,5 +6,5 @@
 // Take a look at the PSR-0-compatible class autoloading implementation
 // in the Readme.php file if you want a simple autoloader setup.
 
-require_once dirname(__FILE__) . '/SmartyPants.php'
-require_once dirname(__FILE__) . '/SmartyPantsTypographer.php'
+require_once dirname(__FILE__) . '/SmartyPants.php';
+require_once dirname(__FILE__) . '/SmartyPantsTypographer.php';
